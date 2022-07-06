60c32361ef73ce12bd9033f294983d03312353ba
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 01:20:34 +0900

    3.8

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const h1 = document.querySelector(".hello h1");
 
 const handleTitleClick = () => {
     const clickedClass = "active"
-    
-    if (h1.classList.contains(clickedClass)) {
-        h1.classList.remove(clickedClass);
-    } else {
-        h1.classList.add(clickedClass);
-    }
+    h1.classList.toggle(clickedClass);
  }
 
 h1.addEventListener("click", handleTitleClick);
