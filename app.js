e07a75187219a060b7a0716da45d38eed3097e4f
jiggy <jiggyjiggy0323@gmail.com>
2022-07-06 22:31:18 +0900

    3.3

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 const title = document.querySelector(".hello h1");
 
 title.innerText = "aaa"
+
+console.dir(title)
+
+// function handleTitleClick() {
+//     title.style.color ="red";
+// }
+
+// const handleTitleClick = () => {console.log("title was clicked")}
+const handleTitleClick = () => {
+    title.style.color ="red"
+    console.log("abc")
+}
+
+title.addEventListener("click", handleTitleClick)
+
+
+
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLHeadingElement
+
