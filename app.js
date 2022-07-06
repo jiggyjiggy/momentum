13f2cae3db8cc6dee75ef4601db7e51c82f75e1b
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 01:23:22 +0900

    4.0; initial settings

--- app.js
+++
@@ -1,8 +0,0 @@
-const h1 = document.querySelector(".hello h1");
-
-const handleTitleClick = () => {
-    const clickedClass = "active"
-    h1.classList.toggle(clickedClass);
- }
-
-h1.addEventListener("click", handleTitleClick);
