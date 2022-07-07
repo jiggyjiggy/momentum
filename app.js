1c65ce7dd5ac4bc2b0b013f0dfd686c54253f230
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 13:57:33 +0900

    4.4

--- app.js
+++ app.js
@@ -14,12 +14,24 @@
 
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting")
+
+const HIDDEN_CLASSNAME = "hidden"
 
 const link = document.querySelector("a")
 
 const onLoginSubmit = (event) => {
     event.preventDefault();
     console.log(event)
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
 }
 
 const handleLinkClick = (event) => {
