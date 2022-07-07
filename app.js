a091f6c828966dad8d8ccf805b076a548732f1e5
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 12:01:56 +0900

    4.1

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const loginButton = document.querySelector("#login-form button")
 
 const onLoginBtnClick = () => {
     const username = loginInput.value;
-    if (username === "") {
-        alert("Please write yout name");
-    } else if (username.length > 15) {
-        alert("Your name is too long")
-    }
+    console.log(username)
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
