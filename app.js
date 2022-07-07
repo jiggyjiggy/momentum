ea67dabb4ef74f8e327815b1fdfb3c32f2f71d2b
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 14:24:43 +0900

    4.6

--- app.js
+++ app.js
@@ -1,44 +1,28 @@
-// const loginForm = document.querySelector("#login-form")
-// const loginInput = loginForm.querySelector("input")
-// const loginButton = loginForm.querySelector("button")
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-// const loginInput = document.querySelector("#login-form input")
-// const loginButton = document.querySelector("#login-form button")
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-// const onLoginBtnClick = () => {
-//     const username = loginInput.value;
-//     console.log(username)
-// }
-
-// loginButton.addEventListener("click", onLoginBtnClick)
-
-const loginForm = document.querySelector("#login-form")
-const loginInput = document.querySelector("#login-form input")
-const greeting = document.querySelector("#greeting")
-
-const HIDDEN_CLASSNAME = "hidden"
-
-const link = document.querySelector("a")
-
-const onLoginSubmit = (event) => {
-    event.preventDefault();
-    console.log(event)
-    const username = loginInput.value;
-    loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
-    localStorage.setItem("username", username)
-    // greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-
-    greeting.classList.remove(HIDDEN_CLASSNAME);
-
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 }
 
-const handleLinkClick = (event) => {
-    event.preventDefault()
-    console.log(event)
-    alert("click")
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
