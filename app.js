8af6eb995625dbc4fa2dbd951cade3509e297082
jiggy <jiggyjiggy0323@gmail.com>
2022-07-07 11:54:01 +0900

    4.1

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const loginInput = document.querySelector("#login-form input")
 const loginButton = document.querySelector("#login-form button")
 
 const onLoginBtnClick = () => {
-    console.log("clicked", loginInput.value)
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write yout name");
+    } else if (username.length > 15) {
+        alert("Your name is too long")
+    }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick)
